f46036a337b31b41b1b77c471ee503d7241053bf
caio-cris <caiohcris@gmail.com>
2022-11-11 13:07:12 -0300

    Funções

---
+++ Fundacao_03/Aula_08/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	valor, err := sum(50, 10)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(valor)
+
+}
+
+func sum(a, b int) (int, error) {
+	if a+b >= 50 {
+		return 0, errors.New("A soma é maior do que 50")
+	} else {
+		return a + b, nil
+	}
+}
