85d2a4c66c5de06c4a78566c3d3b42d844c05655
caio-cris <caiohcris@gmail.com>
2022-11-11 12:04:19 -0300

    Declaração e atribuição

---
+++ Fundacao_03/Aula_02/main.go
@@ -0,0 +1,21 @@
+package main
+
+const a = "Hello, World!"
+
+var (
+	b bool = true
+	c int
+	d string
+	e float64
+)
+
+func main() {
+	a := "primeira atribuição tem :" //string
+	a = "reatribuir a var, não precisa dos :"
+	println(a)
+	println(b)
+}
+
+func x() {
+
+}
