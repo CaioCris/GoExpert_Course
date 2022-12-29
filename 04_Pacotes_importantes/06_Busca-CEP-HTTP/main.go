14110bb00627b7b371dd6ca8bf62135091cefe19
caio-cris <caiohcris@gmail.com>
2022-12-28 23:53:36 -0300

    Criando função para BuscaCEP

--- 04_Pacotes_importantes/06_Busca-CEP-HTTP/main.go
+++ 04_Pacotes_importantes/06_Busca-CEP-HTTP/main.go
@@ -1,8 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 )
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
 func main() {
 	http.HandleFunc("/", SearchCEPHandler)
@@ -22,4 +38,20 @@ func SearchCEPHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hello, world!"))
+}
+
+func SearchCEP(cep string) (*ViaCEP, error) {
+	response, err := http.Get(fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep))
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var cepResponse ViaCEP
+	err = json.Unmarshal(body, &cepResponse)
+	return &cepResponse, nil
 }
