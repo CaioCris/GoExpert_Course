4e5dc32a1bd9e154fcdf8541230d91294eb09168
caio-cris <caiohcris@gmail.com>
2022-12-31 20:27:46 -0300

    Iniciando com templates

---
+++ 04_Pacotes_importantes/09_Templates/template_stdout/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type Course struct {
+	Name     string
+	Workload int
+}
+
+func main() {
+	course := Course{"Go", 40}
+	tmp := template.New("CourseTemplate")
+	tmp, _ = tmp.Parse("Curso: {{.Name}} - Carga Horária: {{.Workload}}")
+	err := tmp.Execute(os.Stdout, course)
+	if err != nil {
+		panic(err)
+	}
+}
